877474b7ffc4cfa18087438697e3f8e8c248bda0
alphia <alphia@163.com>
2021-12-26 14:43:06 +0800

    markpoint on map

--- src/components/middle/Map.js
+++ src/components/middle/Map.js
@@ -14,36 +14,36 @@ echarts.registerMap('zzHiTech', hiTechMap);
 
 
 const ReportGrid = styled(Row)`
-    padding-right: 5%;
-    padding-left: 5%;
-    height: 20%;
+  padding-right: 5%;
+  padding-left: 5%;
+  height: 28%;
 `;
 
 const ReportCol = styled(Col)`
-    box-sizing: border-box;
-    background-color: rgba(14,252,255,0.15) ;
-    height: 5.1rem;
-    justify-content:center;//子元素水平居中
-    align-items:center;//子元素垂直居中
-    display: flex;
-    border-radius: 9px;
+  box-sizing: border-box;
+  background-color: rgba(14, 252, 255, 0.15);
+  height: 5.1rem;
+  justify-content: center; //子元素水平居中
+  align-items: center; //子元素垂直居中
+  display: flex;
+  border-radius: 9px;
 `;
 
 const Label = styled.div`
-    text-align: center;
-    color: #fff;
-    width: 60%;
-    font-size: 1.2rem;
-    border-right: #61dafb 1px;
+  text-align: center;
+  color: #fff;
+  width: 60%;
+  font-size: 1.2rem;
+  border-right: #61dafb 1px;
 `;
 
 const Number = styled.div`
-    text-align: center;
-    color: #0efcff;
-    width: 40%;
-    font-size: 1.5rem;
-    border-left: #fff 1px solid;
-    border-left: #9363ff 1px solid;
+  text-align: center;
+  color: #0efcff;
+  width: 40%;
+  font-size: 1.5rem;
+  border-left: #fff 1px solid;
+  border-left: #9363ff 1px solid;
 `;
 
 function Map(props) {
@@ -55,25 +55,42 @@ function Map(props) {
         return {
             backgroundColor: 'transparent',
             tooltip: {
-                show:true,
+                show: true,
                 formatter: '{b}<br/>{c} (p / km2)'
             },
             series: [
                 {
-                    // name: 'zzHiTech',
                     type: 'map',
                     map: 'zzHiTech',
-                    colorBy: 'data',
                     label: {
-                        show: true
+                        show: true,
+                        fontSize: "1.1rem"
                     },
                     data: [
-                        { name: '沟赵办事处', value: 20057.34, itemStyle:{areaColor:'#be36ff',opacity:0.7} },
-                        { name: '双桥办事处', value: 15477.48, itemStyle:{areaColor:'#2dffbe',opacity:0.7} },
-                        { name: '梧桐办事处', value: 6992.6,itemStyle:{areaColor:'#f0f8ff',opacity:0.7} },
-                        { name: '枫杨办事处', value: 31686.1,itemStyle:{areaColor:'#3299ff',opacity:0.7} },
-                        { name: '石佛办事处', value: 6992.6,itemStyle:{areaColor:'#0efcff',opacity:0.7} },
+                        {name: '沟赵办事处', value: 200, itemStyle: {areaColor: '#be36ff', opacity: 0.7}},
+                        {name: '双桥办事处', value: 154, itemStyle: {areaColor: '#2dffbe', opacity: 0.7}},
+                        {name: '梧桐办事处', value: 6992.6, itemStyle: {areaColor: '#f0f8ff', opacity: 0.7}},
+                        {name: '枫杨办事处', value: 31686.1, itemStyle: {areaColor: '#3299ff', opacity: 0.7}},
+                        {name: '石佛办事处', value: 6992.6, itemStyle: {areaColor: '#0efcff', opacity: 0.7}},
                     ],
+                    markPoint: {
+                        symbol: 'circle',
+                        symbolSize: 40,
+                        label:{
+                            fontSize:'1.1rem',
+                        },
+                        itemStyle:{
+                            color:'#fff',
+                            opacity: 1,
+                        },
+                        data: [
+                            {x:255,y:245,value:200,label:{color:'#be36ff'}},
+                            {x:508,y:85,value:57,label:{color:'#0aaf7b'}},
+                            {x:490,y:315,value:57,label:{color:'#5a6269'}},
+                            {x:570,y:190,value:15,label:{color:'#3299ff'}},
+                            {x:660,y:345,value:75,label:{color:'#02aeb1'}},
+                        ],
+                    }
                 }
             ]
         };
@@ -82,8 +99,8 @@ function Map(props) {
 
     return (
         <div className={className}>
-            <ReportGrid  justify="space-around"  align="middle" gutter={[8,8]}>
-                <ReportCol  span={7}>
+            <ReportGrid justify="space-around" align="middle" gutter={[8, 8]}>
+                <ReportCol span={7}>
                     <Label>群众留<br/>言总数</Label>
                     <Number>75</Number>
                 </ReportCol>
@@ -100,15 +117,15 @@ function Map(props) {
             <ReactECharts
                 option={option}
                 theme={theme}
-                style={{height: '80%', overflow: 'show'}}
+                style={{height: '72%', overflow: 'show'}}
             />
         </div>
     );
 }
 
 export default styled(Map)`
-    height: 69%;
-    margin-bottom: 1%;
+  height: 69%;
+  margin-bottom: 1%;
     //background: url(${rightBg}) no-repeat;
-    background-size:100% 100%;
+  background-size: 100% 100%;
 `
