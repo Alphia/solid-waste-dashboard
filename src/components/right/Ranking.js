0b593d728fca3b23693453e29831a4262d6ec097
alphia <alphia@163.com>
2021-12-29 15:29:51 +0800

    refres right.ranking

--- src/components/right/Ranking.js
+++ src/components/right/Ranking.js
@@ -8,92 +8,102 @@ import {RightReportHeader} from "../Block";
 import _ from 'lodash';
 
 const ReportGrid = styled(Row)`
-    padding-right: 5%;
-    padding-left: 5%;
-    height: 78%;
+  padding-right: 5%;
+  padding-left: 5%;
+  height: 78%;
 `;
 
 const ReportCol = styled(Col)`
-    box-sizing: border-box;
-    background-color: rgba(14,252,255,0.15) ;
-    height: 95%;
-    border-radius: 9px;
+  box-sizing: border-box;
+  background-color: rgba(14, 252, 255, 0.15);
+  height: 95%;
+  border-radius: 9px;
 `;
 
 const Label = styled.div`
-    padding-top: 0.5rem;
-    text-align: center;
-    color: #fff;
-    width: 60%;
-    font-size: 1rem;
-    border-right: #61dafb 1px;
+  padding-top: 0.5rem;
+  text-align: center;
+  color: #fff;
+  width: 60%;
+  font-size: 1rem;
+  border-right: #61dafb 1px;
 `;
 
 const MyDivider = styled(Divider)`
-    margin: 0.2rem;
-    border-block-color: rgba(240,248,255,0.26);
+  margin: 0.2rem;
+  border-block-color: rgba(240, 248, 255, 0.26);
 `;
 
 const ListWrapper = styled.div`
-    padding: 0.3rem 0.6rem 0.3rem 0.3rem;
+  padding: 0.3rem 0.6rem 0.3rem 0.3rem;
 `;
 
 const List = styled.div`
-    display: inline-block;
-    color: white;
-    font-size: 0.9rem;
-    text-align: center;
+  display: inline-block;
+  color: white;
+  font-size: 0.9rem;
+  text-align: center;
 `;
 
 const List1 = styled(List)`
-    width: 20%;
+  width: 20%;
 `;
 const List2 = styled(List)`
-    width: 40%;
+  width: 40%;
 `;
 const List3 = styled(List)`
-    width: 40%;
+  width: 40%;
 `;
 
-const Number = styled.div`
-    display: inline-block;
-    text-align: center;
-    color: #fff;
-    background-color: rgba(14,252,255,0.35);
-    border-radius: 0.6rem;
-    width: 1.2rem;
-    height: 1.2rem;
-    font-size: 0.8rem;
-    
+const CircleNum = styled.div`
+  display: inline-block;
+  text-align: center;
+  color: #fff;
+  background-color: rgba(14, 252, 255, 0.35);
+  border-radius: 0.6rem;
+  width: 1.2rem;
+  height: 1.2rem;
+  font-size: 0.8rem;
 `;
 
 const Text = styled.div`
-    font-size: 0.8rem;
+  font-size: 0.9rem;
+`;
+const Num = styled.div`
+  color: #0efcff;
+  font-size: 1rem;
 `;
 
 function Ranking(props) {
 
     const {className} = props;
-    const data = React.useContext(DataContext);
-    const extractOption = data => {
+    const context = React.useContext(DataContext);
+
+    const extractOption = context => {
+        const rankingByL1 = context.currentyear_aticleCountGroupByOrg_L1;
+        const rankingByL2 = context.currentyear_aticleCountGroupByOrg_L2;
+
+        const insList = _.chain(rankingByL1)
+            .sortBy('cnt')
+            .reverse()
+            .map((e, index) => {
+                return {index: index + 1, name: e.org_name, amount: e.cnt}
+            })
+            .value();
+        const villageList = _.chain(rankingByL2)
+            .sortBy('cnt')
+            .reverse()
+            .map((e, index) => {
+                return {index: index + 1, name: e.org_name, amount: e.cnt}
+            })
+            .value();
+
         return {
-            ins: [
-                {index: 1, name: '沟赵办事处', amount: 11},
-                {index: 2, name: '枫杨办事处', amount: 3},
-                {index: 3, name: '石佛办事处', amount: 2},
-                {index: 4, name: '双桥办事处', amount: 0},
-                {index: 5, name: '梧桐办事处', amount: 0},
-            ],
-            village: [
-                {index: 1, name: '东连河', amount: 8},
-                {index: 2, name: '大里', amount: 4},
-                {index: 3, name: '秋雅', amount: 2},
-                {index: 4, name: '南流', amount: 1},
-                {index: 5, name: '欢河', amount: 1},
-            ],
+            ins: insList,
+            village: villageList,
         };
     };
-    const option = extractOption(data);
+    const option = extractOption(context);
 
     return (
         <div className={className}>
@@ -112,18 +122,20 @@ function Ranking(props) {
                         </div>
                         <MyDivider/>
                         {_.map(option.ins, ins => {
-                                return <div>
-                                    <List1>
-                                        <Number>{ins.index}</Number>
-                                    </List1>
-                                    <List2>
-                                        <Text>{ins.name}</Text>
-                                    </List2>
-                                    <List3>
-                                        <Text>{ins.amount}</Text>
-                                    </List3>
-                                    <MyDivider/>
-                                </div>
+                                return (
+                                    <div>
+                                        <List1>
+                                            <CircleNum>{ins.index}</CircleNum>
+                                        </List1>
+                                        <List2>
+                                            <Text>{ins.name}</Text>
+                                        </List2>
+                                        <List3>
+                                            <Text><Num>{ins.amount}</Num></Text>
+                                        </List3>
+                                        <MyDivider/>
+                                    </div>
+                                )
                             }
                         )}
                     </ListWrapper>
@@ -141,13 +153,13 @@ function Ranking(props) {
                         {_.map(option.village, village =>
                             <>
                                 <List1>
-                                    <Number>{village.index}</Number>
+                                    <CircleNum>{village.index}</CircleNum>
                                 </List1>
                                 <List2>
                                     <Text>{village.name}</Text>
                                 </List2>
                                 <List3>
-                                    <Text>{village.amount}</Text>
+                                    <Text><Num>{village.amount}</Num></Text>
                                 </List3>
                                 <MyDivider/>
                             </>
@@ -160,9 +172,9 @@ function Ranking(props) {
 }
 
 export default styled(Ranking)`
-    height: 31.6%;
-    margin-top: 1rem;
-    background: url(${right}) no-repeat;
-    background-size:100% 100%;
+  height: 31.6%;
+  margin-top: 1rem;
+  background: url(${right}) no-repeat;
+  background-size: 100% 100%;
 `
 
